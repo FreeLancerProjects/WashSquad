397f70991fdd69c3813f6aa52f73a75fb72318b3
Ahmed <ahmedmohamed23345@gmail.com>
2020-08-11 13:23:45 +0200

    ch

--- app/src/main/java/com/creative/share/apps/wash_squad/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/creative/share/apps/wash_squad/adapters/ViewPagerAdapter.java
@@ -5,17 +5,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewPagerAdapter extends FragmentPagerAdapter {
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     private List<Fragment> fragmentList;
     private List<String> titles;
 
     public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+        super(fm);
         fragmentList = new ArrayList<>();
         titles = new ArrayList<>();
 
